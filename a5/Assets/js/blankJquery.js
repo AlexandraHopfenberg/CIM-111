ba0b37edced000d8b3035eb073c62eb687b102ce
Alexandra Hopfenberg <ahh71@miami.edu>
2019-12-03 19:08:43 -0500

    summary

--- a5/Assets/js/blankJquery.js
+++ a5/Assets/js/blankJquery.js
@@ -1,11 +1,24 @@
-$(document).ready(){
+$(document).ready(function(){
 
-console.log("testJquery is online")
+     console.log("test");
 
-}
+   $("button").click(function(){
+   		//console.log("Does this work when i click? ");
+//  $("div").animate({left: '250px'});
+  console.log("Still working?");
+
+      $("div").css("height", "300px");
+   }); //button function
 
 
+   $("div").mouseover(function(){
+   	$("div").css("display", "none");
+   });
 
+   $("div").mouseleave(function(){
+   	$("div").css("display", "block")
+   });
 
+}); //document function 
 
 
